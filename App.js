72d166d67c564db840609cba700b7799a6988f72
greastern <gaiusroy@gmail.com>
2023-03-03 15:17:37 -0500

    basics work

--- App.js
+++ App.js
@@ -1,23 +1,78 @@
+import { useState} from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import {colors} from "./src/constants";
+import {colors, CLEAR, ENTER} from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
+
 
 // Making number of columns which will be number of tries
 const numOfTries = 6;
 
+const copyArray = (arr) => {
+  return [...(arr.map(rows => [...rows]))]
+}
+
 export default function App() {
   const word = "hello";
-  const letters = word.split(''); // Split based on nothing and that will return an array of characters
+  const letters = word.split(""); // Split based on nothing and that will return an array of characters
 
-  // Keep whole map in state. 
-  // The map depends on this array
-  const rows = new Array(numOfTries).fill(
-    new Array(letters.length).fill("a")
-    );
+  const[rows, setRows] = useState(  //[value of state, setter of state]
+      new Array(numOfTries).fill(new Array(letters.length).fill("")) // Keep whole map in state. The map depends on this array
+  );   
+  const [curRow, setCurRow] = useState(0);
+  const[curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+
+    // Clear button going back current columnn one cell behind
+    if (key == CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = "";
+        setRows(updatedRows);
+        setCurCol(prevCol)
+      }
+      return;
+    }
+    // Enter button
+    if (key == ENTER) {
+    // make sure row has been fully filled before going on to next row
+      if (curCol == rows[0].length){
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+      return;
+    }
+    
+   if (curCol < rows[0].length) { //this if statement stops the cursor at the last column
+    // update our rows in const rows
+    updatedRows[curRow][curCol] = key; 
+    setRows(updatedRows);  
+    setCurCol(curCol+1);
+    }
+  };
+
+  const isCellActive = (row,col) => {
+    return row == curRow && col == curCol;
+  };
+
+  const getCellBGColor = (letter, row, col) => {
+    // checking that the row the user is currently in is more or equal to the letter being currently rendered
+    if (row >= curRow) {
+      return colors.black;
+    }
+    if (letter == letters[col]) {    // if the letter is the same as the letter in the position column then its in the right position for green background
+      return colors.primary;
+    } 
+    if (letters.includes(letter)) { //if letter is not guess, then check if word contains has that letter
+      return colors.secondary;
+    }
+    return colors.darkgrey;
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
+   <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
      
      
@@ -27,18 +82,29 @@ export default function App() {
 {/* Make a view, a view of that will contain the whole map */}
       <ScrollView style={styles.map}>
 {/* map for all the rows we have */}
-        {rows.map((row) => (
-          <View style={styles.row}>
-            {row.map((cell) => (
-             <View style={styles.cell}> 
-             <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+        {rows.map((row, i) => (
+          <View key={`row-${i}`} style={styles.row}>
+            {row.map((letter, j) => (
+             <View 
+             key={`cell-${i}-${j}`}
+              style={[
+                styles.cell, 
+                {
+                  borderColor: isCellActive(i,j) 
+                    ? colors.lightgrey 
+                    : colors.darkgrey,
+                  backgroundColor: getCellBGColor(letter, i, j),
+                },
+              ]}
+            > 
+             <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
              </View>
           ))}
         </View>
         ))}
       </ScrollView>
 
-      <Keyboard/>
+      <Keyboard onKeyPressed={onKeyPressed}/>
     </SafeAreaView>
   );
 }
@@ -57,6 +123,7 @@ const styles = StyleSheet.create({
     letterSpacing: 7,
   },
   map: {
+    marginVertical: 20,
     alignSelf: "stretch",
     height: 100,
   },
